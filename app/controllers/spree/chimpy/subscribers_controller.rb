5df25bf55e4370c77c520fb01d643cb54ffd28a2
Denise Fugihara <denise@retroca.com.br>
2015-05-20 19:37:22 -0300

    Added profile and utm variables to Subscriber, and update/create form

--- app/controllers/spree/chimpy/subscribers_controller.rb
+++ app/controllers/spree/chimpy/subscribers_controller.rb
@@ -1,17 +1,39 @@
-class Spree::Chimpy::SubscribersController < ApplicationController
+class Spree::Chimpy::SubscribersController < Spree::BaseController
   # respond_to :html
   layout "spree/layouts/blank"
 
   def create
-    @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
-    @subscriber.update_attributes(params[:chimpy_subscriber])
-    if @subscriber.save
-      Spree::Chimpy::Subscription.new(@subscriber).subscribe
-      flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
-      @success = true
+    @errors = []
+    if params[:chimpy_subscriber][:email].blank?
+      @errors << I18n.t("spree.chimpy.subscriber.missing_email")
+    elsif params[:chimpy_subscriber][:email] !~ /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+      @errors << I18n.t("spree.chimpy.subscriber.missing_email")
     else
-      flash[:error] = I18n.t("spree.chimpy.subscriber.failure")
-      @success = false
+      if Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first
+        @errors <<  params[:chimpy_subscriber]
+        @errors << I18n.t("spree.chimpy.subscriber.already")
+      else
+        @subscriber = Spree::Chimpy::Subscriber.where(email: params[:chimpy_subscriber][:email]).first_or_initialize
+        @subscriber.update_attributes(params[:chimpy_subscriber])
+        @subscriber.profile = params[:user_type]
+        @subscriber.utm_source ||= cookies[:utm_source]
+        @subscriber.utm_campaign ||= cookies[:utm_campaign]
+        @subscriber.utm_medium ||= cookies[:utm_medium]
+        @subscriber.ubdate ||= Date.today
+        if @subscriber.save
+          Spree::Chimpy::Subscription.new(@subscriber).subscribe
+          # user = Spree::User.where(email:params[:chimpy_subscriber][:email]).first
+          # if user
+          #   user.subscribed = true
+          #   user.save
+          # end
+          flash[:newsletter_subscription_tracking] = "nothing special"
+          # flash[:notice] = I18n.t("spree.chimpy.subscriber.success")
+        else
+          @errors <<  params[:chimpy_subscriber]
+          @errors << I18n.t("spree.chimpy.subscriber.failure")
+        end
+      end
     end
 
     respond_to do |format|
@@ -19,5 +41,10 @@ class Spree::Chimpy::SubscribersController < ApplicationController
       format.html
     end
     # respond_with @subscriber, location: request.referer
+  end
+
+  def success
+    flash[:newsletter_subscription_tracking] = "nothing special"
+    render "create"
   end
 end
--- app/models/spree/chimpy/subscriber.rb
+++ app/models/spree/chimpy/subscriber.rb
@@ -1,6 +1,6 @@
 class Spree::Chimpy::Subscriber < ActiveRecord::Base
   self.table_name = "spree_chimpy_subscribers"
 
-  attr_accessible :email, :subscribed
+  attr_accessible :email, :subscribed, :utm_source, :utm_medium, :utm_campaign, :profile, :ubdate
   validates :email, presence: true
 end
