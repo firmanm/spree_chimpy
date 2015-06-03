8f82516dde65014d56c8e6fa0da0b609366be41d
Denise Fugihara <denise@retroca.com.br>
2015-06-03 14:58:41 -0300

    Fix lightbox confirmation

--- app/assets/javascripts/store/spree_chimpy.js
+++ app/assets/javascripts/store/spree_chimpy.js
@@ -1,1 +1,2 @@
 //= require store/spree_core
+//= require store/spree_chimpy_subscriber
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   namespace :chimpy, path: "" do
-    resource :subscribers, only: [:create] do
+    resource :subscribers, :only => :create do
       get 'success', :on => :collection
     end
   end
